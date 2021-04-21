0a087c90b664967f13f5d2a5339221b43e5ce5e1
Filip Povolny <filip.povolny@ondewo.com>
2021-04-21 14:10:17 +0200

    Add services implementation.

--- ondewo/s2t/client/services/speech_to_text.py
+++ ondewo/s2t/client/services/speech_to_text.py
@@ -1,11 +1,67 @@
-from typing import Any
-
-from ondewo.s2t.speech_to_text_pb2_grpc import Speech2TextStub
+from google.protobuf.empty_pb2 import Empty
 from ondewo.utils.base_services_interface import BaseServicesInterface
+
+from ondewo.s2t.speech_to_text_pb2 import (
+    S2tPipelineId,
+    ListS2tDomainsRequest,
+    ListS2tDomainsResponse,
+    ListS2tLanguagesRequest,
+    ListS2tLanguagesResponse,
+    ListS2tPipelinesRequest,
+    ListS2tPipelinesResponse,
+    Speech2TextConfig,
+    TranscribeFileRequest,
+    TranscribeFileResponse,
+    TranscribeStreamRequest,
+    TranscribeStreamResponse,
+)
+from ondewo.s2t.speech_to_text_pb2_grpc import Speech2TextStub
 
 
 class Speech2Text(BaseServicesInterface):
+    """
+    Exposes the s2t endpoints of ONDEWO s2t in a user-friendly way.
+
+    See speech_to_text.proto.
+    """
 
     @property
-    def stub(self) -> Any:
+    def stub(self) -> Speech2TextStub:
         stub: Speech2TextStub = Speech2TextStub(channel=self.grpc_channel)
+        return stub
+
+    def transcribe_file(self, request: TranscribeFileRequest) -> TranscribeFileResponse:
+        response: TranscribeFileResponse = self.stub.TranscribeFile(request)
+        return response
+
+    def transcribe_stream(self, request: TranscribeStreamRequest) -> TranscribeStreamResponse:
+        response: TranscribeStreamResponse = self.stub.TranscribeStream(request)
+        return response
+
+    def get_s2t_pipeline(self, request: S2tPipelineId) -> Speech2TextConfig:
+        config: Speech2TextConfig = self.stub.GetS2tPipeline(request)
+        return config
+
+    def create_s2t_pipeline(self, config: Speech2TextConfig) -> S2tPipelineId:
+        response: S2tPipelineId = self.stub.CreateS2tPipeline(config)
+        return response
+
+    def delete_s2t_pipeline(self, request: S2tPipelineId) -> Empty:
+        response: Empty = self.stub.DeleteS2tPipeline(request)
+        return response
+
+    def update_s2t_pipeline(self, config: Speech2TextConfig) -> Empty:
+        response: Empty = self.stub.UpdateS2tPipeline(config)
+        return response
+
+    def list_s2t_pipelines(self, request: ListS2tPipelinesRequest) -> ListS2tPipelinesResponse:
+        response: ListS2tPipelinesResponse = self.stub.ListS2tPipelines(request)
+        return response
+
+    def list_s2t_languages(self, request: ListS2tLanguagesRequest) -> ListS2tLanguagesResponse:
+        response: ListS2tLanguagesResponse = self.stub.ListS2tLanguages(request)
+        return response
+
+    def list_s2t_domains(self, request: ListS2tDomainsRequest) -> ListS2tDomainsResponse:
+        response: ListS2tDomainsResponse = self.stub.ListS2tDomains(request)
+        return response
